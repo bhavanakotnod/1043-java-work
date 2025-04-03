21638b637b625e65553868b27a4bb3a2962cfcc8
bhavanakotnod <bhavanakotnod@gmail.com>
2025-04-03 16:16:11 +0530

    Add files via upload
    here all programs on the topic of Parameterized constructor .

---
+++ Cons/toString/Car.java
@@ -0,0 +1,26 @@
+package Cons.toString;
+
+public class Car {
+	String brand;
+	String name;
+	String price;
+	public Car(String brand,String name, String  price) {
+		this.brand = brand;
+		this.name = name;
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Car [brand = " + brand + ", name = " + name + ", price = " + price + "]";
+	}
+
+	public static void main(String[] args) {
+		Car c = new Car("Maruti Suzuki","Swift","6.49 L");
+		System.out.println(c);
+		System.out.println("-----------------");
+		Car c1 = new Car("Tata","Punch","6.20L");
+		System.out.println(c1);
+	}
+
+}
