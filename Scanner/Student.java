62bb23ba913167288a044ed32f18ae55c638b670
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-04-09 11:15:22 +0530

    Update Student.java

--- Scanner/Student.java
+++ Scanner/Student.java
@@ -5,17 +5,20 @@ public class Student {
 	
 
 	Scanner sc =new Scanner(System.in) ;
+
+	System.out.println("Enter a roll number");
+	int rollno = sc.nextInt();
+	System.out.println(rollno);
+		
 	System.out.println("Enter a name of the student");
 	String name = sc.nextLine();
 	System.out.println(name);
+	sc.nextLine();
 	
 	System.out.println("Enter Student branch");
 	String branch = sc.nextLine();
 	System.out.println(branch);
 	
-	System.out.println("Enter a roll number");
-	int rollno = sc.nextInt();
-	System.out.println(rollno);
 	
 	System.out.println("Enter a percentage");
     double percentage = sc.nextDouble();
