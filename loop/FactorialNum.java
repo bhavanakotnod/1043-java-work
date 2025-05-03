8c1baaed2e39fa9368c7af893bd5487545066d29
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-05-03 22:37:40 +0530

    Update FactorialNum.java

--- loop/FactorialNum.java
+++ loop/FactorialNum.java
@@ -2,11 +2,11 @@ package com.tka.loop;
 
 public class FactorialNum {
 	public static void main(String[] args) {
-		int n =5;
+		int n =5;          
 		
 		int fact=1;
-		for(int i = 1 ; i <= n ; i++)
-			fact = fact*i ;
+		for(int i = 1 ; i <= n ; i++)      // loop rotated 5 times 
+			fact = fact*i ;           // 1*2*3*4*5  = 120
 		System.out.println(fact);
 	}
 
