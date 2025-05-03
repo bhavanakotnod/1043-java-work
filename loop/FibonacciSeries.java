361de69f60ad5c8c6d7d2c0a42ce934b1b76194b
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-05-03 22:39:20 +0530

    Update FibonacciSeries.java

--- loop/FibonacciSeries.java
+++ loop/FibonacciSeries.java
@@ -2,9 +2,13 @@ package com.tka.loop;
 
 public class FibonacciSeries {
 	public static void main(String[] args) {
+		// first number
 		int a=0;
+		// second number
 		int b= 1;
+		// store addition of two number
 		int c;
+		// series upto 10  number
 		int n =10;
 		for(int i=1; i<=n;++i) {
 			c=a+b;
