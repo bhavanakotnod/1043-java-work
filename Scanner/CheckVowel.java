7db736566e64c217818bc1d8a8cf98f0e03cf83a
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-05-10 22:02:44 +0530

    Update CheckVowel.java

--- Scanner/CheckVowel.java
+++ Scanner/CheckVowel.java
@@ -1,12 +1,15 @@
 package com.Scanner;
 import java.util.Scanner;
+
 public class CheckVowel {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		//taking input from user
 		System.out.println("Enter a Character");
 		char ch = sc.next().charAt(0);
-		
+		//object creation
 		Vowel v = new Vowel();
+		// calling method
 		v.m1(ch);
 		
 		
