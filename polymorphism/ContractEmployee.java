77fd085f249a2779970849da966ba966bd8d2c92
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-07-29 22:37:51 +0530

    ContractEmployee.java

--- polymorphism/ContractEmployee.java
+++ polymorphism/ContractEmployee.java
@@ -1,5 +1,5 @@
 package com.polymorphism;
-
+// child class
 public class ContractEmployee extends PermanantEmployee {
 	public void calculateSalary() {
 		System.out.println("Salary of contract employee  is 7LPA");
