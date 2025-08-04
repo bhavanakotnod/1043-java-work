21a0b10a121c5bd78f43fb5bbf3b0d4b1385aa45
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-08-04 23:14:51 +0530

    Book.java

--- ArgumentConstructor/Book.java
+++ ArgumentConstructor/Book.java
@@ -1,12 +1,15 @@
 package ArgumentConstructor;
 
 public class Book {
+	// Argumented Constructor
 	public Book (String Book_Name,String author,double Price) {
 		System.out.println("Book Name "+ Book_Name);
 		System.out.println("Author of the Book "+author);
 		System.out.println("Price of the book "+ Price);
 	}
+	//main method
 	public static void main(String[] args) {
+		//object creation and parameter passing
 		Book b = new Book("The complete adventures of feluda","Satyajit Ray",200.25);
 		System.out.println("-------------------");
 		Book b1 = new Book("The world as I see it","Albert Einstein",220.25);
@@ -14,3 +17,4 @@ public class Book {
 	}
 
 }
+
