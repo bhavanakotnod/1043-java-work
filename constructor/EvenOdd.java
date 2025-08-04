78c4f2cebe523ac5ba8a2544f7283dfb188227bd
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-08-04 23:11:53 +0530

    EvenOdd.java

--- constructor/EvenOdd.java
+++ constructor/EvenOdd.java
@@ -1,7 +1,7 @@
 package com.constructor;
 
 public class EvenOdd {
-	
+	// constructor
 	public EvenOdd() {
 		int n=54; 
 		if(n%2==0) {
@@ -11,10 +11,13 @@ public class EvenOdd {
 			System.out.println(n+ "  number is odd");
 		}
 	}
+	// parameterized method m1
 	public void m1(int n) {
+		// n > 0 then number is positive
 		if(n>0) {
 			System.out.println(n+" number id positive");
 		}
+			// (-n) < 0 < (+n)
 		else if(n<0) {
 			System.out.println(n+" number is negative");
 		}
@@ -23,10 +26,12 @@ public class EvenOdd {
 		}
 	}
 	
-	
+	//main method
 	public static void main(String[] args) {
+		//object creation
 		EvenOdd e = new EvenOdd();
 		e.m1(5);
 	}
 
 }
+
