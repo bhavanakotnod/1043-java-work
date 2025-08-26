6fcd4383bdf2d617e0cb8611fd094380782a006f
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-08-26 22:30:40 +0530

    ATM.java

--- abstraction/ATM.java
+++ abstraction/ATM.java
@@ -1,11 +1,13 @@
 package abstraction;
 
 public interface ATM {
+	//instance variable
 	public static final double amount = 78500.80;
     	
-	
+	//method
 	void deposite(double amount);
 	public abstract void withDraw(double withdraw_amount);
 	
 
 }
+
